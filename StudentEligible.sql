d973b885a0c8ef0ba2352697d9680168c32c2df6
aaronsh <aaronsh@bbd.co.za>
2022-02-15 15:29:32 +0200

    Updated to include faculties.

--- StudentEligible.sql
+++ StudentEligible.sql
@@ -5,17 +5,20 @@ RETURNS TABLE
 AS
 RETURN
 	SELECT
-		Institutions.[Name] AS 'Institution Name',
-		Qualifications.[Name] AS 'Qualification Name'
+		Institutions.[Name] AS 'Institution',
+		Faculties.[Name] AS 'Faculty',
+		Qualifications.[Name] AS 'Qualification'
 	FROM
 		Institutions,
 		Qualifications,
+		Faculties,
 		Students,
 		Requirements,
 		Results,
 		Subjects
 	WHERE
-		Qualifications.QualificationID = Requirements.QualificationID
+		(Qualifications.QualificationID = Requirements.QualificationID
 		AND Requirements.SubjectID = Subjects.SubjectID AND Requirements.MinimumMark <= Results.Mark
 		AND Results.SubjectID = Subjects.SubjectID
-		AND Results.StudentID = @StudentID
+		AND Results.StudentID = @StudentID)
+		AND Faculties.FacultyID = Qualifications.FacultyID
