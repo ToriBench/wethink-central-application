e1b9959dea0f469eb48395374a132560a536b078
Paulo NGOVE <paulon@bbd.co.za>
2022-02-15 15:46:10 +0200

    updating Creating_all_Views.sql table names to plural

--- Creating_all_Views.sql
+++ Creating_all_Views.sql
@@ -1,17 +1,17 @@
 
-CREATE VIEW Qualification_with_Requirements AS
-SELECT        dbo.Qualification.QualificationID, dbo.Requirements.MinimumMark, dbo.Qualification.MonthDuration, dbo.Subjects.Code, dbo.Subjects.Name, dbo.Institutions.Name AS Institution, dbo.Faculty.Name AS Faculty, 
+CREATE VIEW Qualifications_with_Requirements AS
+SELECT        dbo.Qualifications.QualificationID, dbo.Requirements.MinimumMark, dbo.Qualifications.MonthDuration, dbo.Subjects.Code, dbo.Subjects.Name, dbo.Institutions.Name AS Institution, dbo.Faculties.Name AS Faculty, 
                          dbo.Institutions.ApplicationLink
-FROM            dbo.Qualification INNER JOIN
-                         dbo.Requirements ON dbo.Qualification.QualificationID = dbo.Requirements.QualificationID AND dbo.Qualification.QualificationID = dbo.Requirements.QualificationID INNER JOIN
+FROM            dbo.Qualifications INNER JOIN
+                         dbo.Requirements ON dbo.Qualifications.QualificationID = dbo.Requirements.QualificationID AND dbo.Qualifications.QualificationID = dbo.Requirements.QualificationID INNER JOIN
                          dbo.Subjects ON dbo.Requirements.SubjectID = dbo.Subjects.SubjectID AND dbo.Requirements.SubjectID = dbo.Subjects.SubjectID INNER JOIN
-                         dbo.Faculty ON dbo.Qualification.FacultyID = dbo.Faculty.FacultyID AND dbo.Qualification.FacultyID = dbo.Faculty.FacultyID INNER JOIN
-                         dbo.Institutions ON dbo.Qualification.InstitutionID = dbo.Institutions.InstitutionID AND dbo.Qualification.InstitutionID = dbo.Institutions.InstitutionID
+                         dbo.Faculties ON dbo.Qualifications.FacultyID = dbo.Faculties.FacultyID AND dbo.Qualifications.FacultyID = dbo.Faculties.FacultyID INNER JOIN
+                         dbo.Institutions ON dbo.Qualifications.InstitutionID = dbo.Institutions.InstitutionID AND dbo.Qualifications.InstitutionID = dbo.Institutions.InstitutionID
 
 						 
 
 GO
-CREATE VIEW Student_with_Results AS
+CREATE VIEW Students_with_Results AS
 SELECT        dbo.Users.FirstName, dbo.Users.LastName, dbo.Users.Email, dbo.Subjects.Code AS Subject_Code, dbo.Subjects.Name AS Subject_Name, dbo.Results.Mark
 FROM            dbo.Results INNER JOIN
                          dbo.Students ON dbo.Results.StudentID = dbo.Students.StudentID AND dbo.Results.StudentID = dbo.Students.StudentID INNER JOIN
@@ -25,9 +25,9 @@ FROM            dbo.UserRoles INNER JOIN
                          dbo.Users ON dbo.UserRoles.UserRoleID = dbo.Users.UserRoleID AND dbo.UserRoles.UserRoleID = dbo.Users.UserRoleID
 
 GO
-CREATE VIEW Institution_with_Qualifications AS
-SELECT        dbo.Institutions.Name AS Institution_Name, dbo.Faculty.Name AS Faculty_Name, dbo.Qualification.Name AS Qualification_Name, dbo.Qualification.AP_Score, dbo.Qualification.Description AS Qualification_Descr, 
-                         dbo.Qualification.MonthDuration
-FROM            dbo.Faculty INNER JOIN
-                         dbo.Qualification ON dbo.Faculty.FacultyID = dbo.Qualification.FacultyID AND dbo.Faculty.FacultyID = dbo.Qualification.FacultyID INNER JOIN
-                         dbo.Institutions ON dbo.Qualification.InstitutionID = dbo.Institutions.InstitutionID AND dbo.Qualification.InstitutionID = dbo.Institutions.InstitutionID
+CREATE VIEW Institutions_with_Qualifications AS
+SELECT        dbo.Institutions.Name AS Institution_Name, dbo.Faculties.Name AS Faculty_Name, dbo.Qualifications.Name AS Qualification_Name, dbo.Qualifications.AP_Score, dbo.Qualifications.Description AS Qualification_Descr, 
+                         dbo.Qualifications.MonthDuration
+FROM            dbo.Faculties INNER JOIN
+                         dbo.Qualifications ON dbo.Faculties.FacultyID = dbo.Qualifications.FacultyID AND dbo.Faculties.FacultyID = dbo.Qualifications.FacultyID INNER JOIN
+                         dbo.Institutions ON dbo.Qualifications.InstitutionID = dbo.Institutions.InstitutionID AND dbo.Qualifications.InstitutionID = dbo.Institutions.InstitutionID
