fd674b7b0b41f8c5ae7defd9079ea6935f0ffa13
thaboRach <thaborach@bbd.co.za>
2022-02-22 09:47:36 +0200

    update stored procs

--- DML/StoredProcedures/ProcStudentEligible.sql
+++ DML/StoredProcedures/ProcStudentEligible.sql
@@ -8,7 +8,7 @@ AS
 	SELECT DISTINCT
 		Institutions.[Name] AS 'Institution',
 		Faculties.[Name] AS 'Faculty',
-		Qualifications.[Name] AS 'Qualification'
+		Courses.[Name] AS 'Course'
 	FROM
 		vInstitutionsWithQualifications,
 		Requirements
@@ -16,16 +16,16 @@ AS
 		Subjects ON Subjects.SubjectID = Requirements.SubjectID,
 		vStudentsWithResults
 	WHERE
-		(Students.ApScore >= Qualifications.AP_Score
+		(Students.ApScore >= Courses.AP_Score
 		AND
 		(Results.StudentID = @StudentID 
-		AND Qualifications.QualificationID = Requirements.QualificationID
+		AND Courses.CourseID = Requirements.CourseID
 		AND Results.SubjectID = Requirements.SubjectID
 		AND Requirements.MinimumMark <= Results.Mark))
 		
 		OR
-		Qualifications.QualificationID NOT IN
-		(SELECT Requirements.QualificationID FROM Requirements)
+		Courses.CourseID NOT IN
+		(SELECT Requirements.CourseID FROM Requirements)
 
 	ORDER BY
 		'Institution'
