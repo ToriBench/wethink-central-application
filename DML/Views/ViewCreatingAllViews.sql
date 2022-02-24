59f47c28b1a531f1d36b9b27b2642a25de49aa04
aaronsh <aaronsh@bbd.co.za>
2022-02-24 15:12:00 +0200

    Updated the views with information better suited to the types of queries needed. Added two new views, vCoursesWithNoRequirements and vAddressesWithInstitutions.

--- DML/Views/ViewCreatingAllViews.sql
+++ DML/Views/ViewCreatingAllViews.sql
@@ -1,8 +1,8 @@
 USE WeThinkDB
 GO
 
-CREATE VIEW vCoursesWithRequirements AS
-SELECT        [dbo].Courses.CourseID, [dbo].Requirements.MinimumMark, [dbo].Courses.MonthDuration, [dbo].Subjects.Code, [dbo].Subjects.Name, [dbo].Institutions.Name AS Institution, [dbo].Faculties.Name AS Faculty, 
+ALTER VIEW vCoursesWithRequirements AS
+SELECT        [dbo].Courses.CourseID, [dbo].Requirements.MinimumMark, [dbo].Courses.MonthDuration, [dbo].Subjects.SubjectID, [dbo].Subjects.Name, [dbo].Institutions.Name AS Institution, [dbo].Faculties.Name AS Faculty, 
                          [dbo].Institutions.ApplicationLink
 FROM            [dbo].Courses INNER JOIN
                          [dbo].Requirements ON [dbo].Courses.CourseID = [dbo].Requirements.CourseID INNER JOIN
@@ -14,8 +14,8 @@ FROM            [dbo].Courses INNER JOIN
 
 GO
 
-CREATE VIEW vStudentsWithResults AS
-SELECT        [dbo].Students.FirstName, [dbo].Students.LastName, [dbo].Users.Email, [dbo].Subjects.Code AS Subject_Code, [dbo].Subjects.Name AS Subject_Name, [dbo].Results.Mark
+ALTER VIEW vStudentsWithResults AS
+SELECT       [dbo].Students.StudentID, [dbo].Students.FirstName, [dbo].Students.LastName, [dbo].Users.Email, [dbo].Subjects.SubjectID, [dbo].Subjects.Name AS Subject_Name, [dbo].Results.Mark
 FROM            [dbo].Results INNER JOIN
                          [dbo].Students ON [dbo].Results.StudentID = [dbo].Students.StudentID INNER JOIN
                          [dbo].Subjects ON [dbo].Results.SubjectID = [dbo].Subjects.SubjectID INNER JOIN
@@ -23,16 +23,29 @@ FROM            [dbo].Results INNER JOIN
 
 GO
 
-CREATE VIEW vUsersWithRoles AS
+ALTER VIEW vUsersWithRoles AS
 SELECT        [dbo].Users.Email, [dbo].UserRoles.UserRole
 FROM            [dbo].UserRoles INNER JOIN
                          [dbo].Users ON [dbo].UserRoles.UserRoleID = [dbo].Users.UserRoleID
 
 GO
 
-CREATE VIEW vInstitutionsWithCourses AS
-SELECT        [dbo].Institutions.Name AS Institution_Name, [dbo].Faculties.Name AS Faculty_Name, [dbo].Courses.Name AS Qualification_Name, [dbo].Courses.APScore, [dbo].Courses.Description AS Qualification_Descr, 
+ALTER VIEW vInstitutionsWithCourses AS
+SELECT       [dbo].Courses.CourseID, [dbo].Institutions.Name AS Institution_Name, [dbo].Faculties.Name AS Faculty_Name, [dbo].Courses.Name AS Course_Name, [dbo].Courses.ApScore, [dbo].Courses.Description AS Course_Descr, 
                          [dbo].Courses.MonthDuration
 FROM            [dbo].Faculties INNER JOIN
                          [dbo].Courses ON [dbo].Faculties.FacultyID = [dbo].Courses.FacultyID INNER JOIN
                          [dbo].Institutions ON [dbo].Courses.InstitutionID = [dbo].Institutions.InstitutionID
+GO
+
+CREATE VIEW vCoursesWithNoRequirements AS
+SELECT			[dbo].Courses.CourseID 
+				FROM Courses 
+				WHERE Courses.CourseID NOT IN 
+										(SELECT Requirements.CourseID FROM Requirements)
+
+CREATE VIEW vAddressesWithInstitutions AS
+SELECT			[dbo].Addresses.AddressID, [dbo].Addresses.Province, Students.AddressID, Students.StudentID 
+				FROM Addresses 
+				INNER JOIN Students 
+				ON Addresses.AddressID = Students.AddressID
